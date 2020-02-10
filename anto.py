ee6fe187f1b1d1cbb928f26a00bf41c47fc03c25
NotnaKO <kopanovanton@gmail.com>
2020-02-10 17:20:07 +0300

    first commit

---
+++ anto.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import pygame
+import requests
+
+response = None
+map_request = "http://static-maps.yandex.ru/1.x/?&ll=37.646920%2C55.725146&spn=0.3,0.3&l=map&pt=37.441657%2C55.817159,1~37.560573%2C55.791755,2~37.553017%2C55.715739,3"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
